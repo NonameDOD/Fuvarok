56913ddf09d2be7df029c741142fcb6e6458f235
Fóti Dávid <fotidavid05@gmail.com>
2026-05-10 13:19:27 +0200

    kesz

--- src/main/FuvarokFeladat.java
+++ src/main/FuvarokFeladat.java
@@ -4,19 +4,22 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 public class FuvarokFeladat {
+
     private static List<Fuvar> fuvarok = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         Path path = Path.of("fuvarok.csv");
         List<String> sorok = Files.readAllLines(path);
         sorok.remove(0);
-        
+
         for (String sor : sorok) {
             String[] s = sor.split(",");
-            Fuvar fuvar = new Fuvar(s[0], Integer.parseInt(s[1]), Double.parseDouble(s[2]),s[3]);
+            Fuvar fuvar = new Fuvar(s[0], Integer.parseInt(s[1]), Double.parseDouble(s[2]), s[3]);
             fuvarok.add(fuvar);
         }
 
@@ -26,10 +29,16 @@ public class FuvarokFeladat {
         feladatok();
 
     }
+
     private static void feladatok() {
-        System.out.println("Összes fuvar értéke: "+osszeg());
-        System.out.println("Legdrágább fuvar rendszáma: "+maxRendszam());
-        System.out.println("Legolcsóbb fuvar forintban: "+minOsszegFT()+"FT");
+        System.out.println("Összes fuvar értéke: " + osszeg());
+        System.out.println("Legdrágább fuvar rendszáma: " + maxRendszam());
+        System.out.println("Legolcsóbb fuvar forintban: " + minOsszegFT() + "FT");
+        System.out.println("Hány kártyás fizetés volt: " + kartyasDB());
+        System.out.println("Minden fizetési mód meghatározott: " + fizetesEldontes());
+        System.out.println("Hány darab autó van a rendszerben: " + autoDB());
+        System.out.println("Hányféle fizetési mód van: " + fizetesDB());
+        System.out.println("Melyik autó mennyi fuvart teljesített:\n "+fuvarStatisztika());
     }
 
     private static String maxRendszam() {
@@ -41,6 +50,7 @@ public class FuvarokFeladat {
         }
         return fuvarok.get(maxI).getRendszam();
     }
+
     private static int minOsszegFT() {
         int minI = 0;
         for (int i = 1; i < fuvarok.size(); i++) {
@@ -48,7 +58,7 @@ public class FuvarokFeladat {
                 minI = i;
             }
         }
-        return (int)fuvarok.get(minI).getOsszeg() * 355;
+        return (int) fuvarok.get(minI).getOsszeg() * 355;
     }
 
     private static int osszeg() {
@@ -58,5 +68,56 @@ public class FuvarokFeladat {
         }
         return osszeg;
     }
-    
+
+    private static int kartyasDB() {
+        int osszeg = 0;
+        for (int i = 0; i < fuvarok.size(); i++) {
+            if (fuvarok.get(i).getFizetes().equals("kártya")) {
+                osszeg += fuvarok.get(i).getOsszeg();
+            }
+
+        }
+        return osszeg;
+    }
+
+    private static boolean fizetesEldontes() {
+        int i = 0;
+        while (i < fuvarok.size() && !fuvarok.get(i).getFizetes().equals("-")) {
+            i++;
+        }
+        return i >= fuvarok.size();
+    }
+
+    private static int autoDB() {
+        HashSet<String> autoDb = new HashSet<>();
+        for (int i = 0; i < fuvarok.size(); i++) {
+            autoDb.add(fuvarok.get(i).getFizetes());
+        }
+        return autoDb.size();
+    }
+
+    private static int fizetesDB() {
+        HashSet<String> autoDb = new HashSet<>();
+        for (int i = 0; i < fuvarok.size(); i++) {
+            autoDb.add(fuvarok.get(i).getFizetes());
+        }
+        return autoDb.size();
+    }
+
+    private static String fuvarStatisztika() {
+        HashMap<String, Integer> stat = new HashMap<>();
+        for (Fuvar fuvar : fuvarok) {
+            String rendszam = fuvar.getRendszam();
+            if (stat.containsKey(rendszam)) {
+                stat.put(rendszam, stat.get(rendszam) + 1);
+            } else {
+                stat.put(rendszam, 1);
+            }
+        }
+        String eredmeny = "";
+        for (String rendszam : stat.keySet()) {
+            eredmeny += rendszam + ": " + stat.get(rendszam) + " fuvar\n";
+        }
+        return eredmeny;
+    }
 }
